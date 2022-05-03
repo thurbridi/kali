c2d513f5b1357a5c39c6db9a1ef3dc29a541daef
Arthur Bridi Guazzelli <arthur.b.guazzelli@gmail.com>
2022-05-03 11:41:51 -0300

    activities persistence in local storage

--- src/actions/activities.ts
+++ src/actions/activities.ts
@@ -3,39 +3,65 @@ import { v4 as uuidv4 } from 'uuid';
 
 import { AppDispatch } from "../store/store";
 
-export const activityAdded = (title: string, description: string, sourceId: string): Action => ({
+export const activityAdded = (activity: Activity): Action => ({
     type: 'activities/activityAdded',
-    payload: {
+    payload: activity
+})
+
+export const activityAddedAsync = (activityData: Partial<Activity> = {}) => {
+    // Activity defaults
+    const activity: Activity = {
         id: uuidv4(),
-        title,
-        description,
+        title: '',
+        description: '',
         startDate: undefined,
         endDate: undefined,
         dueDate: undefined,
-        sourceId,
+        sourceId: undefined,
         status: 'Backlog',
+        rank: undefined,
+        isArchived: false,
+        tags: undefined,
+        ...activityData
     }
-})
+    return (dispatch: AppDispatch) => {
+        return window.storageAPI.storeKey(`state.activities.${activity.id}`, activity)
+            .then(() => dispatch(activityAdded(activity)))
+    }
+}
 
 export const activityEdited = (activity: Activity) => ({
     type: 'activities/activityEdited',
-    payload: {
-        ...activity
-    }
+    payload: activity
 })
+
+export const activityEditedAsync = (activity: Activity) => {
+    return (dispatch: AppDispatch) => {
+        return window.storageAPI.storeKey(`state.activities.${activity.id}`, activity)
+            .then(() => dispatch(activityAdded(activity)))
+    }
+}
 
 export const activityRemoved = (id: string) => ({
     type: 'activities/activityRemoved',
     payload: id
 })
 
+export const activityRemovedAsync = (id: string) => {
+    return async (dispatch: AppDispatch) => {
+        return window.storageAPI.deleteKey(`state.activities.${id}`)
+            .then(() => dispatch(activityRemoved(id)))
+    }
+}
+
 const activitiesFetched = (activities: { [id: string]: Activity }) => ({
     type: 'activities/activitiesFetched',
     payload: activities
 })
 
-export const activitiesFetchedAsync = () => (dispatch: AppDispatch) => {
-    return window.storageAPI.loadKey('state.activities').then((activities) => {
-        dispatch(activitiesFetched(activities))
-    })
+export const activitiesFetchedAsync = () => {
+    return async (dispatch: AppDispatch) => {
+        return window.storageAPI.loadKey('state.activities')
+            .then((activities) => dispatch(activitiesFetched(activities)))
+    }
 }
