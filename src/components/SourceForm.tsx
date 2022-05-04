17951f9547256d4dc071076dfc2e798f3fdf34dc
Arthur Bridi Guazzelli <arthur.b.guazzelli@gmail.com>
2022-05-04 18:33:07 -0300

    Activities list in source detail screen

--- src/components/SourceForm.tsx
+++ src/components/SourceForm.tsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
-import type { Source } from '../types/types';
+import type { Activity, Source } from '../types/types';
 import { connect, ConnectedProps } from 'react-redux'
-import { sourceAddedAsync, sourceEdited, sourceEditedAsync } from '../actions/sources';
-import { AppDispatch } from '../store/store';
+import { sourceAddedAsync, sourceEditedAsync } from '../actions/sources';
+import { AppDispatch, AppState } from '../store/store';
 import { colorSwatch1 } from '../colorSwatches';
+import ActivityListItem from './ActivityListItem';
+import ActivityForm from "./ActivityForm";
+import Modal from 'react-modal';
 
 interface Props extends PropsFromRedux {
     onSubmit: (event: React.FormEvent<HTMLFormElement>) => void
@@ -13,6 +16,14 @@ interface Props extends PropsFromRedux {
 const SourceForm = (props: Props) => {
     const colorIdx = Math.floor(Math.random() * (colorSwatch1.length - 1))
     const defaultColor = colorSwatch1[colorIdx]
+
+    const [openActivity, setOpenActivity] = useState(false)
+
+    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        setOpenActivity(false)
+    }
+
 
     const [title, setTitle] = useState(props.sourceItem ? props.sourceItem.title : '')
     const [description, setDescription] = useState(props.sourceItem ? props.sourceItem.description : '')
@@ -31,36 +42,67 @@ const SourceForm = (props: Props) => {
     }
 
     return (
-        <form onSubmit={(event) => {
-            props.sourceItem ? props.sourceEditedAsync({ ...props.sourceItem, title, description, color }) : props.sourceAddedAsync({ title, description })
-            props.onSubmit(event)
-        }}>
-            <div className='source-form__color-banner' style={{ height: "10rem", background: color }}>
+        <div>
+            <form onSubmit={(event) => {
+                props.sourceItem ? props.sourceEditedAsync({ ...props.sourceItem, title, description, color }) : props.sourceAddedAsync({ title, description, color })
+                props.onSubmit(event)
+            }}>
+                <div className='source-form__color-banner' style={{ height: "10rem", background: color }}>
+                    <input
+                        type='text'
+                        value={color}
+                        onChange={onColorChange}
+                        placeholder='#'
+                        style={{ maxWidth: "7rem", background: color, color: "#fafafa" }}
+                    />
+                </div>
                 <input
+                    className='title'
                     type='text'
-                    value={color}
-                    onChange={onColorChange}
-                    placeholder='#'
-                    style={{ maxWidth: "7rem", background: color, color: "#fafafa" }}
+                    value={title}
+                    onChange={onTitleChange}
+                    placeholder='Source title'
                 />
-            </div>
-            <input
-                className='title'
-                type='text'
-                value={title}
-                onChange={onTitleChange}
-                placeholder='Source title'
-            />
-            <textarea
-                className='detail'
-                value={description}
-                onChange={onDescriptionChange}
-                placeholder='Description'
-            />
+                <textarea
+                    className='detail'
+                    value={description}
+                    onChange={onDescriptionChange}
+                    placeholder='Description'
+                />
 
-            <button>{props.sourceItem ? 'Save' : 'Add'}</button>
-        </form>
+                <button>{props.sourceItem ? 'Save' : 'Add'}</button>
+            </form>
+            {
+                props.sourceItem ?
+                    <div>
+                        <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+                            <h3>Activities</h3>
+                            <button onClick={() => setOpenActivity(true)}>+</button>
+                        </div>
+                        <div className='activityList__content'>
+                            {
+                                props.sourceActivities.map((activity: Activity) =>
+                                    <ActivityListItem key={activity.id} activity={activity} />
+                                )
+                            }
+                        </div>
+                        <Modal isOpen={openActivity} onRequestClose={() => setOpenActivity(false)}>
+                            <ActivityForm source={props.sourceItem} onSubmit={onSubmit} />
+                        </Modal>
+                    </div>
+                    : null
+            }
+        </div>
     )
+}
+
+const mapStateToProps = (state: AppState, props: any) => {
+    const sourceActivities = props.sourceItem ? Object.values(state.activities).filter((activity: Activity) =>
+        activity.sourceId === props.sourceItem.id
+    ) : null
+    return {
+        sourceActivities
+    }
 }
 
 const mapDispatchToProps = (dispatch: AppDispatch) => ({
@@ -68,7 +110,7 @@ const mapDispatchToProps = (dispatch: AppDispatch) => ({
     sourceEditedAsync: (source: Source) => dispatch(sourceEditedAsync(source))
 })
 
-const connector = connect(undefined, mapDispatchToProps)
+const connector = connect(mapStateToProps, mapDispatchToProps)
 
 type PropsFromRedux = ConnectedProps<typeof connector>
 
--- src/components/SourceListItem.tsx
+++ src/components/SourceListItem.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react"
 import type { Source } from '../types/types'
 import Modal from 'react-modal';
-import ActivityForm from "./ActivityForm";
 import SourceForm from "./SourceForm";
 import { sourceRemovedAsync } from "../actions/sources";
 import { connect, ConnectedProps } from "react-redux";
@@ -15,12 +14,11 @@ interface Props extends PropsFromRedux {
 
 const SourceListItem = (props: Props) => {
     const [open, setOpen] = useState(false)
-    const [openActivity, setOpenActivity] = useState(false)
+
 
     const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault()
         setOpen(false)
-        setOpenActivity(false)
     }
 
     const onRemove = () => {
@@ -36,15 +34,10 @@ const SourceListItem = (props: Props) => {
                     <p>{props.numCompletedActivities}/{props.numActivities} Completed</p>
                 </div>
                 <div>
-                    <button onClick={onRemove}>Remove Source</button>
-                    <button onClick={() => setOpenActivity(true)}>Add activity</button>
+                    <button onClick={onRemove}>Remove</button>
                 </div>
                 <Modal isOpen={open} onRequestClose={() => setOpen(false)}>
                     <SourceForm onSubmit={onSubmit} sourceItem={props.source} />
-                </Modal>
-
-                <Modal isOpen={openActivity} onRequestClose={() => setOpenActivity(false)}>
-                    <ActivityForm source={props.source} onSubmit={onSubmit} />
                 </Modal>
             </div>
         </div >
